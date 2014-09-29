d7cfd72e08c1b007580f627c7f3ccdc1f98124ce
benyafilm <filmbenya@FilmBenyas-MacBook-Pro.local>
2014-09-29 15:08:31 +0700

    comment 2

---
+++ pac.rb
@@ -0,0 +1,20 @@
+puts ("Welcome to Intro to ICE.");
+puts ("Welcome to Intro to ICE.");
+puts ("Welcome to Intro to ICE.");
+puts ("Welcome to Intro to ICE.");
+print "First Line"; puts "Second line"
+
+#example of hwo to define function
+def print_hello_world()
+	puts "Hello World"
+end
+
+#example of how to call a function
+print_hello_world(); #parens and ; optional
+print_hello_world;
+print_hello_world
+
+# set a variable
+book = "Harry Potter"
+puts book
+
